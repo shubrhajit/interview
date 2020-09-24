bfbce0e71784b83b8351498b688e659d41c3dfd0
Shubrhajit Nandy <shubrhajitnandy@Shubrhajits-MacBook-Pro.local>
2020-09-24 22:45:37 +0530

    First bind

--- src/app/api-call.service.ts
+++ src/app/api-call.service.ts
@@ -6,15 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiCallService {
   private apikey = '13231eb1b55eeaed3307a9ceb26cfc09';
-  private apiurl =  'api.openweathermap.org/data/2.5/weather?q=';
+  private apiurl =  'http://api.openweathermap.org/data/2.5/weather?q=';
   // preparedUrl = 
   constructor( private http:HttpClient ) { 
 
   }
   getCityWeather(city:string){
-    this.http.get(this.apiurl+city+'&appid='+this.apikey).subscribe((data)=>{
-      return data;
-    })
+    return this.http.get<object>(this.apiurl+city+'&appid='+this.apikey);
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from './api-call.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'interview';
   apikey = '13231eb1b55eeaed3307a9ceb26cfc09';
+  data:object;
+  constructor(private api:ApiCallService){
+    this.cityChange();
+  }
+  
+  cityChange(city='chennai'){
+    this.api.getCityWeather(city).subscribe((d)=>{
+      console.log(d);
+      this.data = d;
+    })
+  }
+    
   
 }
